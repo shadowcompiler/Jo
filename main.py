b1a3a85425f2839d7991cfcf33ba4e18cadbfeeb
shadowcompiler <pariso03henri@gmail.com>
2020-09-24 12:47:56 +0100

    - Reformat code, add keyboard markup

--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 # coding:utf-8
 
 # import
-
-from telegram.ext import (Updater, CommandHandler)
-from utils.token import key
+from telegram.ext import (Updater, CommandHandler, CallbackQueryHandler)
+from telegram import (InlineKeyboardButton, InlineKeyboardMarkup)
 from utils.start import message
-import logging
+from utils.log import log
+from utils.token import key
 
 # import
 
-updater = Updater(token=key, use_context=True)  # updater init
-dispatcher = updater.dispatcher
+log()
 
-"""
-log generator : For know when and why things don't work
-"""
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-"""
-Jo functions
-"""
+# Jo functions
 
 
 def start(update, context):
-    """
-    start reply process
-    """
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message.format(update.effective_user.first_name))
+    keyboard = [[InlineKeyboardButton('Résumé du jour', callback_data='1'),
+                 InlineKeyboardButton('Histoire du jour', callback_data='2')],
+
+                [InlineKeyboardButton('Bouquinez', url='https://bouquinez.com'),
+                 InlineKeyboardButton('Découvrir...', callback_data='3')],
+                [InlineKeyboardButton('Aide', callback_data='4')],
+                ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text(message.format(update.effective_chat.first_name), reply_markup=reply_markup)
 
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-# END
+def button(update, context):
+    query = update.callback_query
+
+    query.answer()
+    if query.data == '1':
+
+        query.edit_message_text(text="Selected option: {}".format(query.data))
 
 
-def start(update, context):
-    """
-    start reply process
-    """
+def main():
+    updater = Updater(token=key, use_context=True)  # updater init
+    dispatcher = updater.dispatcher
+
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
+    # Start the bot
+    updater.start_polling()
 
 
-# Bot start
-updater.start_polling()
+main()
